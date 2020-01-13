12955b3359cb7805d205c38f9756e46354fd7d1b
allenjli <allenjli@umich.edu>
2020-01-13 00:22:54 -0500

    successful splitting of data

--- data_cleaning.R
+++ data_cleaning.R
@@ -21,16 +21,21 @@ c_glu <- read.csv("C:/Users/typer321/Documents/cgm_glucose_biost699.csv", header
 #above target BG > 180 and 250)
 
 c_glu2 <- c_glu %>% rename(glucose = Historic.Glucose.mg.dL.) %>% 
-  dplyr::mutate(Time24 = format(strptime(Time, "%I:%M %p"), format="%H:%M")) %>% 
-  dplyr::mutate(combined = format(strptime(paste(Date,Time24), "%m/%d/%Y %H:%M"))) %>%
-  dplyr::select(-c(Time24,Date,Time)) %>%
-  group_by(ID) %>% dplyr::mutate(init = first(combined)) %>%
-  dplyr::mutate(delta_time = as.numeric(difftime(combined,init))/60)
+  dplyr::mutate(combined = format(strptime(paste(Date,Time), "%m/%d/%Y %I:%M %p"))) %>%
+  dplyr::select(-c(Date,Time)) %>%
+  group_by(ID) %>% dplyr::arrange(combined) %>% dplyr::mutate(init = first(combined)) %>%
+  dplyr::mutate(delta_time = as.numeric(difftime(combined,init))/3600) %>% 
+  filter(ID == 21 | ID == 36)
+#336 hours = 2 weeks, since each glucose measurement period was 2 weekss
+c_glu_separate <- c_glu2 %>% mutate(period = cut(delta_time, breaks= c(-Inf,336,Inf),labels = c(1,2))) %>%
+  group_by(ID,period) %>% dplyr::arrange(combined) %>% dplyr::mutate(init = first(combined)) %>%
+  dplyr::mutate(delta_time_group = as.numeric(difftime(combined,init))/3600) 
 
-#delta_time is in minutes
+#delta_time is in hours
 
 #adding exploratory variables here 
 #time past variable
+#Time in Range
 #AUC variable(?) 
 #above target variable
 #below target variable
@@ -50,7 +55,10 @@ c_dem <- c_dem %>% rename(ID = id)
 merge_attempt <- inner_join(summary_stats, c_dem, by = "ID")
 
 #by_id
-test_plot <- ggplot(data = c_glu2, aes(x = Time, y = glucose, color = ID)) + 
+test_plot <- ggplot(data = c_glu2, aes(x = delta_time, y = glucose, color = ID)) + 
+  geom_line(aes(group = ID))
+
+test_plot2 <- ggplot(data = c_glu_separate, aes(x = delta_time_group, y = glucose, color = ID)) + 
   geom_line(aes(group = ID))
 
 #new var, time post initial measurement
