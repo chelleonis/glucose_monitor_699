92a8b92f0b2586d30d7fdedd4652a893667e6ea4
allenjli <allenjli@umich.edu>
2020-01-21 04:39:20 -0500

    adding plots

--- data_cleaning.R
+++ data_cleaning.R
@@ -77,7 +77,7 @@ short_summary <- merge_attempt_4 %>% group_by(ID,drug_type) %>%
             time_bt_2 = sum(dt_group[d_bt_2 < 0]),
             auc_at_1 = sum(dt_group[d_at_1 > 0]*gluc_avg[d_at_1>0]),
             auc_at_2 = sum(dt_group[d_at_2 > 0]*gluc_avg[d_at_2>0]),
-            auc_bt_1 = sum(dt_group[d_at_1 < 0]*gluc_avg[d_at_1 < 0]),
+            auc_bt_1 = sum(dt_group[d_bt_1 < 0]*gluc_avg[d_bt_1 < 0]),
             auc_bt_2 = sum(dt_group[d_bt_2 < 0]*gluc_avg[d_bt_2 < 0]),
             mean_gl = mean(glucose))
 
@@ -88,12 +88,34 @@ merge_attempt_5 <- inner_join(short_summary, c_dem, by = "ID")
 
 #c_dem2 <- c_dem %>% select(c(ID,Treatment.group))
 
-summary(aov(auc_bt_2 ~ drug_type, merge_attempt_5))
+summary(aov(auc_bt_2~drug_type, data=  merge_attempt_5))
+summary(aov(time_bt_2~drug_type, data=  merge_attempt_5))
+
 summary(lm(log(auc_bt_2+1) ~ Age, data = merge_attempt_5))
 
+
+#50 and 29
+id_50 <- merge_attempt_4 %>% filter(ID == 50) %>% mutate(dt_init = dt_init/24)
 #by_id
-#test_plot <- ggplot(data = c_glu2, aes(x = delta_time, y = glucose, color = ID)) + 
-#  geom_line(aes(group = ID))
+test_plot <- ggplot(data = id_50, aes(x = dt_init, y = glucose, color = drug_type)) + 
+  geom_line(aes(group = drug_type)) +
+  labs(x = "Time (Days)", y = "Glucose Level", title = "Continuous Glucose Monitoring Plot for subject 50") +
+  geom_hline(yintercept = 250, linetype="dashed", color = "darkred") +
+  geom_hline(yintercept = 54, linetype="dashed", color = "darkred") +
+  geom_hline(yintercept = 180, linetype="dashed", color = "orange") +
+  geom_hline(yintercept = 70, linetype="dashed", color = "orange") +
+  theme(plot.title = element_text(hjust = 0.5, size = 18), axis.text = element_text(size = 12), 
+        axis.title = element_text(size = 15)) +
+  scale_x_continuous(limits = c(0,14), breaks = c(0,7,14)) +
+  scale_color_manual(values=c("#0000ff", "#00CC00"))
+
+#wtih 2 lines, make another ver with 4 lines
+
+#ADD MEAN WITHIN RANGE CATEGORICAL VARIABLE
+
+test_plot
+
+ggsave("hmm.png", plot = test_plot)
 
 #test_plot2 <- ggplot(data = c_glu_separate, aes(x = delta_time_group, y = glucose, color = ID)) + 
 #  geom_line(aes(group = ID))
